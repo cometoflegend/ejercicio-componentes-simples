39c866498521b1103c1fdd42364c82c0d382720b
cometoflegend <probablyfriege@gmail.com>
2024-11-04 21:38:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import React from 'react';
-import './App.css';
+import Contador from './Ejercicio1.js';
+
 
 function App() {
 
-
-  const coches = [{ Matricula: '1425BTY', Marca: 'Peugeot', Modelo: '3008', Tipo: 'híbrido' },
-
-  { Matricula: '1762MNY', Marca: 'Mercedes', Modelo: 'EQS', Tipo: 'Diesel' },
-
-  { Matricula: '9882TPK', Marca: 'Renault', Modelo: '4 E-Tech', Tipo: 'Eléctrico' },
-
-  { Matricula: '6634TRV', Marca: 'Volswagen', Modelo: 'Golf', Tipo: 'Gasolina' },
-
-  { Matricula: '2285RPL', Marca: 'Toyota', Modelo: 'RAV4', Tipo: 'hibrido' },
-
-  { Matricula: '1782PRT', Marca: 'Peugeot', Modelo: '5008', Tipo: 'diesel' }]
-
   return (
-    <>
+  <>
+    <Contador/>
+    
+  </>
 
-
-      {coches.map(function (coche) {
-        return (
-          <div className="StyledTextComponent" key={coche}>
-            <p>{coche.Matricula} - {coche.Modelo} {coche.Marca} ({coche.Tipo})</p>
-          </div>
-
-        )
-      })}
-      
-    </>
-
-  );
+  )
 
 };
 
---
+++ src/Ejercicio1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react';
+
+function Contador() {
+  const [contador, setContador] = useState(0);
+
+  return (
+    <div>
+      <p>Contador: {contador}</p>
+      <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+      <button onClick={() => setContador(contador - 1)}>Decrementar</button>
+    </div>
+  );
+}
+
+export default Contador;
